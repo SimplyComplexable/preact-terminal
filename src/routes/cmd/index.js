5d747d94e6addf066633f5c41d11c9425f24f92e
zsunderland <Simplycomplexable@gmail.com>
2017-10-16 07:55:42 -0600

    Add cd and ls commands
    Create exported object which contains all of the routes available within
    the website. Use this object to enable cd and ls features on the cmd
    line. Import preact-helmet for page titles and head data.

--- src/routes/cmd/index.js
+++ src/routes/cmd/index.js
@@ -1,86 +1,111 @@
 import { h, Component } from 'preact';
+import route from 'preact-router';
 import style from './style';
-import Line from "./line";
+import Line from './line';
+import routes from 'async!../routes';
+
+// const routes = {
+// 	home: '/'
+// };
 
 export default class Cmd extends Component {
 
-    commands = {
-        cd: function () {
-            console.log(arguments);
-        }
-    };
+				commands = {
+					cd(...args) {
+						console.log(routes['home']);
+						if (args.length > 0) {
+							try {
+								route.route(routes[args[0]], false);
+							}
+							catch (e) {
+								return `bash: The file or directory ${args[0]} does not exist.`;
+							}
+						}
+					},
+					ls(...args) {
+						if (args.length === 0) {
+							let output = '';
+							for (let key in routes) {
+								output += routes[key] + ' ';
+							}
+							return output;
+						}
+					}
+				};
 
-    constructor() {
-        super();
-        this.state = {
-            lines: [
-                {
-                    position: [
-                        '~'
-                    ],
-                    cmd: ''
-                }
-            ],
-            userContent: ''
-        };
-    }
+				constructor() {
+					super();
+					this.state = {
+						lines: [
+							{
+								position: [
+									'~'
+								],
+								cmd: ''
+							}
+						],
+						userContent: ''
+					};
+				}
 
-    runCommand(cmd) {
-        let operation = cmd.splice(0,1);
-        if (this.commands[operation] === undefined) {
-            return `${operation}: command not found`
-        }
-        return this.commands[operation](...cmd);
-    }
+				runCommand(cmd) {
+					let operation = cmd.splice(0,1);
+					if (this.commands[operation] === undefined) {
+						return `${operation}: command not found`;
+					}
+					return this.commands[operation](...cmd);
+				}
 
-    handleCommand(cmdString) {
-        let cmd = cmdString.split(' ');
-        let cmdResponse = this.runCommand(cmd);
-        let lines = this.state.lines.slice();
+				handleCommand(cmdString) {
+					let cmd = cmdString.split(' ');
+					let cmdResponse = this.runCommand(cmd);
+					let lines = this.state.lines.slice();
 
-        let newline = {
-            position: this.state.lines[this.state.lines.length -1].position,
-            cmd: cmdString,
-            response: cmdResponse
-        };
-        lines.splice(lines.length - 1, 0, newline);
-        return lines;
-    }
+					let newline = {
+						position: this.state.lines[this.state.lines.length -1].position,
+						cmd: cmdString,
+						response: cmdResponse
+					};
+					lines.splice(lines.length - 1, 0, newline);
+					return lines;
+				}
 
-    handleKeyPress(e) {
-        e.preventDefault();
-        e.stopPropagation();
+				handleKeyPress(e) {
+					e.preventDefault();
+					e.stopPropagation();
         
-        let content = this.state.userContent;
-        let lines = this.state.lines;
+					let content = this.state.userContent;
+					let lines = this.state.lines;
 
-        //send command if enter is pressed
-        if (e.key === 'Enter') {
-            lines = this.handleCommand(content);
-            content = '';
-        // override backspace functionality
-        } else if (e.key === 'Backspace') {
-            content = content.substr(0,content.length - 1);
+					//send command if enter is pressed
+					if (e.key === 'Enter') {
+						lines = this.handleCommand(content);
+						content = '';
+						// override backspace functionality
+					}
+					else if (e.key === 'Backspace') {
+						content = content.substr(0,content.length - 1);
 
-        // only print to screen if key is a standard value, (e.g. not Control, Alt, etc.)
-        } else if (e.key.length === 1) {
-            content = content + e.key;
-        }
-        this.setState({
-            lines,
-            userContent: content
-        });
-    }
+						// only print to screen if key is a standard value, (e.g. not Control, Alt, etc.)
+					}
+					else if (e.key.length === 1) {
+						content = content + e.key;
+					}
+					this.setState({
+						lines,
+						userContent: content
+					});
+				}
 
-    render(props, state) {
-        let lines = state.lines.slice();
-        let currentPosition = lines.pop().position;
-        return (
-            <div class={style.main} tabIndex="1" onKeyDown={ this.handleKeyPress.bind(this) } >
-                { lines.map(line => <Line line={ line } />) }
-                <span>{currentPosition.join('\\') }$ </span>
-                { state.userContent }
-            </div>
-        );
-    }
+				render(props, state) {
+					let lines = state.lines.slice();
+					let currentPosition = lines.pop().position;
+					return (
+						<div class={style.main} tabIndex="1" onKeyDown={this.handleKeyPress.bind(this)} >
+							{ lines.map(line => <Line line={line} />) }
+							<span>{currentPosition.join('\\') }$ </span>
+							{ state.userContent }
+						</div>
+					);
+				}
 }
